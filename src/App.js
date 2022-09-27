9f2b89a1490e6e16b00c715a8101c275d5769d90
ArtExpress <105834340+ArtExpress@users.noreply.github.com>
2022-09-26 23:43:17 -0700

    react-airbnb-clone

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from "./components/Home"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import SearchPage from './components/SearchPage';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+          <Routes>
+
+            <Route exact path="/" element={<Home />} />
+            <Route  path="/search" element={<SearchPage />} />
+         
+          </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import "./Search.css"
+import { DateRangePicker } from "react-date-range"
+import "react-date-range/dist/styles.css"
+import "react-date-range/dist/theme/default.css"
+import PeopleIcon from '@mui/icons-material/People';
+import { Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+function Search() {
+  const navigate = useNavigate()
+
+  const [startDate, setStartDate] = useState
+  (new Date());
+  const [endDate, setEndDate] = useState
+  (new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key:"selection",
+  }
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  return (
+    <div className="search">
+    <DateRangePicker 
+      ranges={[selectionRange]} 
+      onChange={handleSelect} 
+    />
+    <h2>
+      Number of guests <PeopleIcon />
+    </h2>
+    <input 
+      defaultValue={2}
+      type="number"
+      min={0}
+    />
+    <Button onClick={() => navigate('/search')}>Search Airbnb</Button>
+    </div>
+  )
+}
+
+export default Search
